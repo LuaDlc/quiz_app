f75f4ef33a6605da6e74394ff583d1ed826d9029
Luana Dias <luana.dias@colab.re>
2023-02-13 22:36:01 -0300

    aplicativo criado

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/page/quiz_app_page.dart';
+
+void main() => runApp(QuizApp());
---
+++ lib/page/quiz_app_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  QuizPage({Key? key}) : super(key: key);
+
+  List<Icon> marcadorDePontos = [
+    const Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    const Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    const Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    const Icon(
+      Icons.close,
+      color: Colors.red,
+    )
+  ];
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'As perguntas serão exibidas aqui.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.white),
+              child: const Text(
+                'Verdadeiro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão verdadeiro.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style:
+                  TextButton.styleFrom(backgroundColor: Colors.grey.shade800),
+              child: const Text(
+                'Falso',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão falso.
+              },
+            ),
+          ),
+        ),
+        Row(),
+      ],
+    );
+  }
+}
+
+/*
+pergunta1: 'O metrô é um dos meios de transporte mais seguros do mundo', verdadeiro,
+pergunta2: 'A culinária brasileira é uma das melhores do mundo.', verdadeiro,
+pergunta3: 'Vacas podem voar, assim como peixes utilizam os pés para andar.', falso,
+*/
